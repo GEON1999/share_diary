78af0d82b80ab8dcdb242783a5c44ee851c2d889
geon <phgst@playdonut.com>
2023-09-12 20:33:29 +0900

    todo & diary list markup

--- src/components/table/DiaryTable.js
+++ src/components/table/DiaryTable.js
@@ -2,18 +2,28 @@ import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { useMutation } from "@tanstack/react-query";
 import useCalendarQuery from "@/Query/useCalendarQuery";
+import styled from "styled-components";
+
+const Diary = styled.div`
+  width: 300px;
+  height: 50px;
+  text-align: left;
+  border-bottom: 2px solid #273838;
+  outline: none;
+  padding-left: 10px;
+  color: #ffffff;
+  margin: 15px 0px;
+`;
 
 const DiaryTable = ({ diaryData }) => {
   return (
     <>
       {diaryData?.map((data) => {
         return (
-          <div key={data.id} className={"text-black"}>
+          <Diary key={data.id} className={"text-black"}>
             <p>제목 : {data?.title}</p>
             <p>내용 : {data?.content}</p>
-            <p>작성자 : {data?.user?.name}</p>
-            <p>작성일 : {data?.createdAt}</p>
-          </div>
+          </Diary>
         );
       })}
     </>
