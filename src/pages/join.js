b575359ba2f17323b7eafa474afc320b33ad2694
geon <phgst@playdonut.com>
2023-09-19 23:15:22 +0900

    join markup

--- src/pages/join.js
+++ src/pages/join.js
@@ -3,6 +3,51 @@ import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import styled from "styled-components";
+
+const JoinContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 640px;
+  height: 620px;
+
+  margin: 0 auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  background-color: rgba(59, 59, 59, 0.5);
+  border-radius: 30px;
+  padding: 130px 0;
+`;
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  border-bottom: 1px solid #000000;
+  outline: none;
+  padding-left: 10px;
+  color: #000000;
+  margin: 10px 0px;
+`;
+
+const SubmitBtn = styled.button`
+  background-color: rgba(25, 25, 112, 0.5);
+  color: #ffffff;
+  width: 100px;
+  height: 50px;
+  border-radius: 10px;
+  margin-top: 20px;
+`;
 
 const Join = () => {
   const router = useRouter();
@@ -32,38 +77,38 @@ const Join = () => {
       }*/
 
   return (
-    <div className={"w-full p-20"}>
+    <JoinContainer>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div
+        <FormContainer
           className={
             'flex flex-col space-y-5 "w-full" justify-center content-center items-center mt-40'
           }
         >
           {" "}
-          <input
+          <Input
             className={"w-80 rounded text-black"}
             {...register("id", { required: true })}
             type="text"
             placeholder="아이디"
           />
-          <input
+          <Input
             className={"w-80 rounded text-black"}
             {...register("pw", { required: true })}
             type="password"
             placeholder="비밀번호"
           />
-          <input
+          <Input
             className={"w-80 rounded text-black"}
             {...register("pwConfirm", { required: true })}
             type="password"
             placeholder="비밀번호 재확인"
           />
-          <button className={"bg-blue-50 px-3 py-1 rounded-2xl text-black"}>
+          <SubmitBtn className={"bg-blue-50 px-3 py-1 rounded-2xl text-black"}>
             join
-          </button>
-        </div>
+          </SubmitBtn>
+        </FormContainer>
       </form>
-    </div>
+    </JoinContainer>
   );
 };
 
