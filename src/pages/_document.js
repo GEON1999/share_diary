ccddd6e13e9884aa060a05f74ef531c6842c17b2
geon <phgst@playdonut.com>
2023-10-23 20:48:13 +0900

    applied SSR to styled component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import Document, { DocumentContext } from "next/document";
+import { ServerStyleSheet } from "styled-components"; // styled-components SSR 적용하기 위함. 공식 문서 참고.
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <>
+                <App {...props} />
+              </>
+            ),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (error) {
+      throw error;
+    } finally {
+      sheet.seal();
+    }
+  }
 }
+
+export default MyDocument;
